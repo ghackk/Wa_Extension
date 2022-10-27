4fd28519165a1936e93b175c77db33bda06612ac
ghackk <gyaneshkumar6520.com>
2022-10-27 23:54:00 +0530

    Removed one member

--- local_host.bat
+++
@@ -1,3 +0,0 @@
-@echo off
-cd "C:\Program Files\Google\Chrome\Application"
-.\chrome.exe --remote-debugging-port=6520 --user-data-dir="C:\Users\gyane\OneDrive\Desktop\5\data_science_lab\web automation\local data"
